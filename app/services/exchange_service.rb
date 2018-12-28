c3a934cea40c04f4ba05f3726b5f01f00fb7f8c2
Mynssem <rafaelmynssem@id.uff.br>
2018-12-28 01:30:51 -0200

    conversão com Bitcoin

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -11,13 +11,29 @@ class ExchangeService
  
   def perform
     begin
-      exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
-      exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
-      url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
-      res = RestClient.get url
-      value = JSON.parse(res.body)['currency'][0]['value'].to_f
-      
-      value * @amount
+      if @target_currency == 'BTC'
+        if @target_currency == @source_currency
+            @amount
+        else
+            url = "https://blockchain.info/tobtc?currency=#{@source_currency}&value=#{@amount}"
+            res = RestClient.get url
+        end
+      else
+        if @source_currency == 'BTC'
+            valor=50000.to_f
+            url = "https://blockchain.info/tobtc?currency=#{@target_currency}&value=#{valor}"
+            res = RestClient.get url
+            value = (@amount * valor) / res.to_f
+        else
+            exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
+            exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
+            url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
+            res = RestClient.get url
+            value = JSON.parse(res.body)['currency'][0]['value'].to_f
+            
+            value * @amount
+        end
+      end
     rescue RestClient::ExceptionWithResponse => e
       e.response
     end
