71c045651c51df27bbd11d32440a5882a5496326
Mynssem <rafaelmynssem@id.uff.br>
2018-12-28 15:06:17 -0200

    Helper modificado para o select do form mostrar o nome das moedas

--- app/helpers/exchanges_helper.rb
+++ app/helpers/exchanges_helper.rb
@@ -2,10 +2,21 @@ module ExchangesHelper
 
     def currency_list
       [
-        'AUD', 'BGN', 'BRL', 'BTC', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP',
-        'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN',
-        'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD',
-        'THB', 'TRY', 'USD', 'ZAR'
+        ['Dólar Australiano', 'AUD'],['Lev Búlgaro','BGN'],['Real', 'BRL'],['Bitcoin','BTC'],
+        ['Dólar Canadense','CAD'],['Franco Suíço','CHF'],['Renminbi','CNY'],['Coroa Checa','CZK'],
+        ['Coroa Dinamarquesa','DKK'],['Euro','EUR'],['Libra Esterlina','GBP'],['Dólar de Hong Kong','HKD'],
+        ['Kuna Croata','HRK'],['Florim Húngaro','HUF'],['Rupia Indonésia','IDR'],['Novo Shekel Israelense','ILS'],
+        ['Rupia Indiana','INR'],['Iene','JPY'],['Won Sul-Coreano','KRW'],['Peso Mexicano','MXN'],
+        ['Ringgit Malaio','MYR'],['Coroa Norueguesa','NOK'],['Dólar Neozelandês','NZD'],['Peso Filipino','PHP'],
+        ['Zloty','PLN'],['Leu Romeno','RON'],['Rublo Russo','RUB'],['Coroa Sueca','SEK'],
+        ['Dólar de Singapura','SGD'],['Baht','THB'],['Lira Turca','TRY'],['Dólar dos Estados Unidos','USD'],
+        ['Rand','ZAR']      
       ]
+    #  [
+    #    'AUD', 'BGN', 'BRL', 'BTC', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP',
+    #    'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN',
+    #    'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD',
+    #    'THB', 'TRY', 'USD', 'ZAR'
+    #  ]
     end
   end
